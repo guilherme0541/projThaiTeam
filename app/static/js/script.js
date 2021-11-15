f78ab5da85b7685d8626fa635b209e56f248a01d
Ressiley <ressiley@hotmail.com>
2021-11-14 22:42:15 -0300

    Ajuste no layout

---
+++ app/static/js/script.js
@@ -0,0 +1,14 @@
+(function () {
+    'use strict'
+    
+    let btn = document.querySelector("#btn");
+    let sidebar = document.querySelector(".sidebar");
+    let searchBtn = document.querySelector(".bx-search");
+
+    btn.onclick = function(){
+        sidebar.classList.toggle("active");
+    }
+    searchBtn.onclick = function(){
+        sidebar.classList.toggle("active");
+    }
+})()
