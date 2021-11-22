c1aa345796af1f454a165c703771a36c80f0dc60
Ressiley <ressiley@hotmail.com>
2021-11-22 00:26:04 -0300

    Add layout e funcionalidade da tela de planos

---
+++ app/models/plano.py
@@ -0,0 +1,8 @@
+class Plano():
+    def __init__(self, id, descricao, valor, descricaoModalidade, ativo):
+        self.id = id
+        self.descricao = descricao
+        self.valor = valor
+        self.descricaoModalidade = descricaoModalidade
+        self.ativo = ativo
+
